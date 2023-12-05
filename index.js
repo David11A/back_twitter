e86644670c54971bf1a98357f15c60e76fb11834
DAVID11 <93211987+David11A@users.noreply.github.com>
2023-12-04 21:43:27 -0600

    commit

---
+++ index.js
@@ -0,0 +1,35 @@
+import  Express  from "express";
+const app = Express();
+import { auth } from "./routes/auth.js";
+import { dataExample } from "./routes/dataExample.js";
+import { users } from "./routes/users.js";
+import { posts } from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+import path from 'path';
+import cors from 'cors';
+
+
+
+app.use(Express.json());
+app.use(cookieParser());
+app.use(Express.static('public'));
+const corsOptions = {
+    origin : 'http://localhost:5173/', 
+    credentials : true
+}
+
+app.use(cors(corsOptions));
+app.use('/api/Auth', auth);
+app.use('/api/DataExample', dataExample);
+app.use('/api/Users', users);
+app.use('/api/Posts', posts);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join( 'public', 'index.html'));
+  });
+
+
+
+
+app.listen(4000, ()=>{
+
+})
